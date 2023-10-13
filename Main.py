431eb06b6f1c81eec8f6b74e163f8237c897f979
AaronWUSN <adwalker.usn@gmail.com>
2023-10-13 15:50:36 -0600

    rearrange main window

--- Main.py
+++ Main.py
@@ -1,15 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-def MainWindow():
-	app = QApplication([])
-	window = QWidget()
-	xsize = 1000
-	ysize = 800
-	window.setGeometry(0,0,xsize,ysize)
-	window.setWindowTitle("First Try")
-
-	window.show()
-	app.exec()
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle("First Try")
+		xsize = 800
+		ysize = 600
+		self. setGeometry(0,0,xsize,ysize)
 	
-MainWindow()
+app = QApplication(sys.argv)
+	
+window = MainWindow()
+window.show()
+	
+app.exec()	
