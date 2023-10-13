039f92352b3f43e8780c05c71ad7284ee8c7904a
AaronWUSN <adwalker.usn@gmail.com>
2023-10-13 11:47:03 -0600

    Test Commit for graph

--- Main.py
+++ Main.py
@@ -4,8 +4,8 @@ from PyQt5.QtWidgets import QApplication, QLabel, QWidget
 def MainWindow():
 	app = QApplication([])
 	window = QWidget()
-	xsize = 800
-	ysize = 600
+	xsize = 1000
+	ysize = 800
 	window.setGeometry(0,0,xsize,ysize)
 	window.setWindowTitle("First Try")
 
